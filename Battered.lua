d16a6eda7e39b38e06647315466f517514de2f8f
Battered2 <battered2@gmail.com>
2024-11-06 13:36:18 -0500

    still updating...

--- Battered.lua
+++ Battered.lua
@@ -104,90 +104,6 @@ function Battered:GetSpell(name)
 	return false
 end		
 
-
-function Battered:Original()
-	local c = CastSpellByName
-	if UnitClass("player") == "Warrior" then 
-	
-		Battered:AutoAttack()
-		
-		--Prot Stance
-		--Stay in Prot, Sunder to 5
-		local _,_,isActive = GetShapeshiftFormInfo(2)
-		if isActive then 
-			if Battered:GetSpell("Bloodrage") and UnitAffectingCombat("player") and UnitMana("player") < 40 and not Battered:OnCooldown("Bloodrage") then 
-				c("Bloodrage") 
-				return
-			end
-			if Battered:GetSpell("Revenge") and not Battered:OnCooldown("Revenge") and UnitMana("player") > 4 then
-				c("Revenge")
-			end
-			if Battered:GetSpell("Bloodthirst") and not Battered:OnCooldown("Bloodthirst") and UnitMana("player") > 29 then
-				c("Bloodthirst")
-				return
-			end
-
-			if not Battered:GetBuff("target","Sunder Armor") then
-				c("Sunder Armor")
-			else
-				if Battered:GetBuff("target","Sunder Armor",1) < 5 then
-					c("Sunder Armor")
-				end
-			end
-
-			if Battered:GetSpell("Heroic Strike") and UnitMana("player") > 30 then
-				c("Heroic Strike")
-			end
-			return
-		end
-
-		--Execute After Prot Stance so rage is not blown on a target before another, switch stance to execute or call manually
-		if (UnitHealth("target") / UnitHealthMax("target")) <= 0.2 and UnitMana("player") > 9 then 
-			c("Execute") 
-			return
-		end
-
-		--If rage under 30 and dodged attack, swap and over power
-		if GetTime()-Battered_Settings["dodge"] < 5 then
-			if Battered:GetSpell("Overpower") and not Battered:OnCooldown("Overpower") and UnitMana("player") < 30 and UnitMana("player") > 4 then
-				c("Battle Stance");
-				c("Overpower")
-			end
-		end
-		
-		c("Berserker Stance")
-		
-		if Battered:GetSpell("Battle Shout") and not Battered:GetBuff("player","Battle Shout") then 
-			if UnitExists("target") and (UnitHealth("target") / UnitHealthMax("target")) > 0.2 and UnitMana("player") > 9 then
-				c("Battle Shout") 
-				return
-			elseif not UnitExists("target") and UnitMana("player") > 9 then
-				c("Battle Shout") 
-				return
-			end
-		end
-		
-		if Battered:GetSpell("Bloodrage") and UnitAffectingCombat("player") and UnitMana("player") < 40 and not Battered:OnCooldown("Bloodrage") then 
-			c("Bloodrage") 
-			return
-		end
-		
-		if Battered:GetSpell("Bloodthirst") and not Battered:OnCooldown("Bloodthirst") and UnitMana("player") > 29 then
-			c("Bloodthirst")
-			return
-		elseif Battered:GetSpell("Whirlwind") and not Battered:OnCooldown("Whirlwind") and UnitMana("player") > 29 then 
-			if CheckInteractDistance("target", 1 ) ~= nil then 
-				c("Whirlwind") 
-				return
-			end 
-		elseif Battered:GetSpell("Heroic Strike") and UnitMana("player") > 29 then
-				c("Heroic Strike")
-		end			
-	
-	end
-end
-
-
 function Battered:AutoAttack()
 	for i=1,120 do 
 		if IsCurrentAction(i) then 
@@ -229,7 +145,6 @@ function Battered:Sweeping()
 end
 
 
-
 function Battered:BatteredCleave()
 	local c = CastSpellByName
 	if UnitClass("player") == "Warrior" then 
@@ -256,135 +171,6 @@ function Battered:BatteredCleave()
 	
 		c("Cleave")
 		return
-	end
-end
-
-function Battered:BatteredZerk(sunders)
-	local c = CastSpellByName
-	if UnitClass("player") == "Warrior" then 
-	
-		Battered:AutoAttack()
-		
-		--Execute After Prot Stance so rage is not blown on a target before another, switch stance to execute or call manually
-		if (UnitHealth("target") / UnitHealthMax("target")) <= 0.2 and UnitMana("player") > 9 then 
-			c("Execute") 
-			return
-		end
-
-		--If rage under 30 and dodged attack, swap and over power
-		if GetTime()-Battered_Settings["dodge"] < 5 then
-			if Battered:GetSpell("Overpower") and not Battered:OnCooldown("Overpower") and UnitMana("player") < 30 and UnitMana("player") > 4 then
-				c("Battle Stance");
-				c("Overpower")
-				return
-			end
-		end
-		
-		c("Berserker Stance")
-		
-		if Battered:GetSpell("Battle Shout") and not Battered:GetBuff("player","Battle Shout") then 
-			if UnitExists("target") and (UnitHealth("target") / UnitHealthMax("target")) > 0.2 and UnitMana("player") > 9 then
-				c("Battle Shout") 
-				return
-			elseif not UnitExists("target") and UnitMana("player") > 9 then
-				c("Battle Shout") 
-				return
-			end
-		end
-		
-		if(sunders > 0) then
-			if not Battered:GetBuff("target","Sunder Armor") then
-				c("Sunder Armor")
-			else
-				if Battered:GetBuff("target","Sunder Armor",1) < sunders then
-					c("Sunder Armor")
-				end
-			end
-		end
-		
-		if Battered:GetSpell("Bloodrage") and UnitAffectingCombat("player") and UnitMana("player") < 40 and not Battered:OnCooldown("Bloodrage") then 
-			c("Bloodrage") 
-			return
-		end
-		
-		if Battered:GetSpell("Bloodthirst") and not Battered:OnCooldown("Bloodthirst") and UnitMana("player") > 29 then
-			c("Bloodthirst")
-			return
-		elseif Battered:GetSpell("Whirlwind") and not Battered:OnCooldown("Whirlwind") and UnitMana("player") > 29 then 
-			if CheckInteractDistance("target", 1 ) ~= nil then 
-				c("Whirlwind") 
-				return
-			end 
-		elseif Battered:GetSpell("Heroic Strike") and UnitMana("player") > 29 then
-				c("Heroic Strike")
-				return
-		end		
-	end
-end
-
-function Battered:BatteredZerk0()
-	Battered:BatteredZerk(0)
-end
-
-function Battered:BatteredZerk2()
-	Battered:BatteredZerk(2)
-end
-
-function Battered:BatteredZerk5()
-	Battered:BatteredZerk(5)
-end
-
-function Battered:BatteredDef()
-	local c = CastSpellByName
-	if UnitClass("player") == "Warrior" then 
-	
-		Battered:AutoAttack()
-		
-		--If rage under 30 and dodged attack, swap and over power
-		if GetTime()-Battered_Settings["dodge"] < 5 then
-			if Battered:GetSpell("Overpower") and not Battered:OnCooldown("Overpower") and UnitMana("player") < 30 and UnitMana("player") > 4 then
-				c("Battle Stance");
-				c("Overpower")
-			end
-		end
-		
-		c("Defensive Stance")
-		
-		if Battered:GetSpell("Battle Shout") and not Battered:GetBuff("player","Battle Shout") then 
-			if UnitExists("target") and (UnitHealth("target") / UnitHealthMax("target")) > 0.2 and UnitMana("player") > 9 then
-				c("Battle Shout") 
-				return
-			elseif not UnitExists("target") and UnitMana("player") > 9 then
-				c("Battle Shout") 
-				return
-			end
-		end
-				
-		if Battered:GetSpell("Bloodrage") and UnitAffectingCombat("player") and UnitMana("player") < 40 and not Battered:OnCooldown("Bloodrage") then 
-			c("Bloodrage") 
-			return
-		end
-		
-		if not Battered:GetBuff("target","Sunder Armor") then
-			c("Sunder Armor")
-		else
-			if Battered:GetBuff("target","Sunder Armor",1) < 5 then
-				c("Sunder Armor")
-			end
-		end
-		
-		if Battered:GetSpell("Bloodthirst") and not Battered:OnCooldown("Bloodthirst") and UnitMana("player") > 29 then
-			c("Bloodthirst")
-			return
-		elseif Battered:GetSpell("Heroic Strike") and UnitMana("player") > 29 then
-				c("Heroic Strike")
-		end	
-		
-		if Battered:GetSpell("Revenge") and not Battered:OnCooldown("Revenge") and UnitMana("player") > 4 then
-			c("Revenge")
-			return
-		end
-		
 	end
 end
 
@@ -452,6 +238,123 @@ function Battered:BatteredArms(sunders)
 end
 
 
+function Battered:BatteredFury0()
+	Battered:BatteredZerk(0)
+end
+
+function Battered:BatteredFury2()
+	Battered:BatteredZerk(2)
+end
+
+function Battered:BatteredFury5()
+	Battered:BatteredZerk(5)
+end
+
+function Battered:BatteredFurysunders)
+	local c = CastSpellByName
+	if UnitClass("player") == "Warrior" then 
+	
+		Battered:AutoAttack()
+		
+		--Execute After Prot Stance so rage is not blown on a target before another, switch stance to execute or call manually
+		if (UnitHealth("target") / UnitHealthMax("target")) <= 0.2 and UnitMana("player") > 9 then 
+			c("Execute") 
+			return
+		end
+
+		--If rage under 30 and dodged attack, swap and over power
+		if GetTime()-Battered_Settings["dodge"] < 5 then
+			if Battered:GetSpell("Overpower") and not Battered:OnCooldown("Overpower") and UnitMana("player") < 30 and UnitMana("player") > 4 then
+				c("Battle Stance");
+				c("Overpower")
+				return
+			end
+		end
+		
+		c("Berserker Stance")
+		
+		Battered:Shout()
+		
+		if(sunders > 0) then
+			if not Battered:GetBuff("target","Sunder Armor") then
+				c("Sunder Armor")
+			else
+				if Battered:GetBuff("target","Sunder Armor",1) < sunders then
+					c("Sunder Armor")
+				end
+			end
+		end
+		
+		if Battered:GetSpell("Bloodrage") and UnitAffectingCombat("player") and UnitMana("player") < 40 and not Battered:OnCooldown("Bloodrage") then 
+			c("Bloodrage") 
+			return
+		end
+		
+		if Battered:GetSpell("Bloodthirst") and not Battered:OnCooldown("Bloodthirst") and UnitMana("player") > 29 then
+			c("Bloodthirst")
+			return
+		elseif Battered:GetSpell("Whirlwind") and not Battered:OnCooldown("Whirlwind") and UnitMana("player") > 29 then 
+			if CheckInteractDistance("target", 1 ) ~= nil then 
+				c("Whirlwind") 
+				return
+			end 
+		elseif Battered:GetSpell("Heroic Strike") and UnitMana("player") > 29 then
+				c("Heroic Strike")
+				return
+		end		
+	end
+end
+
+
+function Battered:BatteredDef()
+	local c = CastSpellByName
+	if UnitClass("player") == "Warrior" then 
+	
+		Battered:AutoAttack()
+		
+		--If rage under 30 and dodged attack, swap and over power
+		if GetTime()-Battered_Settings["dodge"] < 5 then
+			if Battered:GetSpell("Overpower") and not Battered:OnCooldown("Overpower") and UnitMana("player") < 30 and UnitMana("player") > 4 then
+				c("Battle Stance");
+				c("Overpower")
+			end
+		end
+		
+		c("Defensive Stance")
+		
+		Battered:Shout()
+				
+		if Battered:GetSpell("Bloodrage") and UnitAffectingCombat("player") and UnitMana("player") < 40 and not Battered:OnCooldown("Bloodrage") then 
+			c("Bloodrage") 
+			return
+		end
+		
+		if not Battered:GetBuff("target","Sunder Armor") then
+			c("Sunder Armor")
+		else
+			if Battered:GetBuff("target","Sunder Armor",1) < 5 then
+				c("Sunder Armor")
+			end
+		end
+		
+		if Battered:GetSpell("Bloodthirst") and not Battered:OnCooldown("Bloodthirst") and UnitMana("player") > 29 then
+			c("Bloodthirst")
+			return
+		elseif Battered:GetSpell("Heroic Strike") and UnitMana("player") > 29 then
+				c("Heroic Strike")
+		end	
+		
+		if Battered:GetSpell("Revenge") and not Battered:OnCooldown("Revenge") and UnitMana("player") > 4 then
+			c("Revenge")
+			return
+		end
+		
+	end
+end
+
+
+
+
 SlashCmdList['BATTERED_AUTOATTACK_SLASH'] = Battered.AutoAttack
 SLASH_BATTERED_AUTOATTACK_SLASH1 = '/BatteredAutoAttack'
 
@@ -470,14 +373,14 @@ SLASH_BATTERED_ARMS2_SLASH1 = '/BatteredArms2'
 SlashCmdList['BATTERED_ARMS5_SLASH'] = Battered.BatteredArms5
 SLASH_BATTERED_ARMS5_SLASH1 = '/BatteredArms5'
 
-SlashCmdList['BATTERED_ZERK0_SLASH'] = Battered.BatteredZerk0
-SLASH_BATTERED_ZERK0_SLASH1 = '/BatteredZerk0'
+SlashCmdList['BATTERED_FURY_SLASH'] = Battered.BatteredFury0
+SLASH_BATTERED_FURY0_SLASH1 = '/BatteredFury0'
 
-SlashCmdList['BATTERED_ZERK2_SLASH'] = Battered.BatteredZerk2
-SLASH_BATTERED_ZERK2_SLASH1 = '/BatteredZerk2'
+SlashCmdList['BATTERED_FURY_SLASH'] = Battered.BatteredFury2
+SLASH_BATTERED_FURY2_SLASH1 = '/BatteredFury2'
 
-SlashCmdList['BATTERED_ZERK5_SLASH'] = Battered.BatteredZerk5
-SLASH_BATTERED_ZERK5_SLASH1 = '/BatteredZerk5'
+SlashCmdList['BATTERED_FURY5_SLASH'] = Battered.BatteredFury5
+SLASH_BATTERED_FURY5_SLASH1 = '/BatteredFury5'
 
 SlashCmdList['BATTERED_DEF_SLASH'] = Battered.BatteredDef
 SLASH_BATTERED_DEF_SLASH1 = '/BatteredDef'
