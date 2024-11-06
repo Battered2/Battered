2f499225980c300519895cfd9bbc122321c69654
Battered2 <battered2@gmail.com>
2024-11-06 12:41:26 -0500

    Update for new skills

--- Battered.lua
+++ Battered.lua
@@ -104,16 +104,6 @@ function Battered:GetSpell(name)
 	return false
 end		
 
-function Battered:AutoAttack()
-	for i=1,120 do 
-		if IsCurrentAction(i) then 
-			return 
-		end 
-	end 
-	CastSpellByName("Attack")
-	return
-end
-
 
 function Battered:Original()
 	local c = CastSpellByName
@@ -198,26 +188,64 @@ function Battered:Original()
 end
 
 
+function Battered:AutoAttack()
+	for i=1,120 do 
+		if IsCurrentAction(i) then 
+			return 
+		end 
+	end 
+	CastSpellByName("Attack")
+	return
+end
+
+
+function Battered:Shout()
+	if UnitClass("player") == "Warrior" then 
+		if Battered:GetSpell("Battle Shout") and not Battered:GetBuff("player","Battle Shout") then 
+			if UnitExists("target") and (UnitHealth("target") / UnitHealthMax("target")) > 0.2 and UnitMana("player") > 9 then
+				CastSpellByName("Battle Shout") 
+				return
+			elseif not UnitExists("target") and UnitMana("player") > 9 then
+				CastSpellByName("Battle Shout") 
+				return
+			end
+		end
+	end
+	return
+end
+
+function Battered:Sweeping()
+	if UnitClass("player") == "Warrior" then 
+		if Battered:GetSpell("Sweeping Strikes") and not Battered:GetBuff("player","Sweeping Strikes") then 
+		
+			if UnitMana("player") > 19 then
+				CastSpellByName("Battle Stance") 
+				CastSpellByName("Sweeping Strikes") 
+				return
+			end	
+		end
+	end
+	return
+end
+
+
+
 function Battered:BatteredCleave()
 	local c = CastSpellByName
 	if UnitClass("player") == "Warrior" then 
 	
 		Battered:AutoAttack()
 			
-		if Battered:GetSpell("Battle Shout") and not Battered:GetBuff("player","Battle Shout") then 
-			if UnitExists("target") and (UnitHealth("target") / UnitHealthMax("target")) > 0.2 and UnitMana("player") > 9 then
-				c("Battle Shout") 
-				return
-			elseif not UnitExists("target") and UnitMana("player") > 9 then
-				c("Battle Shout") 
-				return
-			end
-		end
+		Battered:Shout()
+		
+		Battered:Sweeping()
 		
 		if Battered:GetSpell("Bloodrage") and UnitAffectingCombat("player") and UnitMana("player") < 40 and not Battered:OnCooldown("Bloodrage") then 
 			c("Bloodrage") 
 			return
 		end
+		
+		c("Berserker Stance")
 		
 		if Battered:GetSpell("Whirlwind") and not Battered:OnCooldown("Whirlwind") and UnitMana("player") > 24 then 
 			if CheckInteractDistance("target", 1 ) ~= nil then 
@@ -361,8 +389,86 @@ function Battered:BatteredDef()
 end
 
 
+function Battered:BatteredArms0()
+	Battered:BatteredArms(0)
+end
+
+function Battered:BatteredArms2()
+	Battered:BatteredArms(2)
+end
+
+function Battered:BatteredArms5()
+	Battered:BatteredArms(5)
+end
+
+function Battered:BatteredArms(sunders)
+	local c = CastSpellByName
+	if UnitClass("player") == "Warrior" then 
+	
+		Battered:AutoAttack()
+		
+		if (UnitHealth("target") / UnitHealthMax("target")) <= 0.2 and UnitMana("player") > 80 then 
+			c("Execute") 
+			return
+		end
+		
+		if GetTime()-Battered_Settings["dodge"] < 5 then
+			if Battered:GetSpell("Overpower") and not Battered:OnCooldown("Overpower") and UnitMana("player") < 30 and UnitMana("player") > 4 then
+				c("Battle Stance");
+				c("Overpower")
+				return
+			end
+		end
+		
+		Battered:Sweeping()
+			
+		c("Berserker Stance")
+		
+		Battered:Shout()
+		
+		if(sunders > 0) then
+			if not Battered:GetBuff("target","Sunder Armor") then
+				c("Sunder Armor")
+			else
+				if Battered:GetBuff("target","Sunder Armor",1) < sunders then
+					c("Sunder Armor")
+				end
+			end
+		end
+		
+		if Battered:GetSpell("Bloodrage") and UnitAffectingCombat("player") and UnitMana("player") < 40 and not Battered:OnCooldown("Bloodrage") then 
+			c("Bloodrage") 
+			return
+		end
+		
+		if Battered:GetSpell("Mortal Strike") and not Battered:OnCooldown("Mortal Strike") and UnitMana("player") > 29 then
+			c("Mortal Strike")
+			return
+		elseif Battered:GetSpell("Slam") and UnitMana("player") > 29 then
+			c("Slam")
+			return
+		end		
+	end
+end
+
+
+SlashCmdList['BATTERED_AUTOATTACK_SLASH'] = Battered.AutoAttack
+SLASH_BATTERED_AUTOATTACK_SLASH1 = '/BatteredAutoAttack'
+
+SlashCmdList['BATTERED_SHOUT_SLASH'] = Battered.Shout
+SLASH_BATTERED_SHOUT_SLASH1 = '/BatteredShout'
+
 SlashCmdList['BATTERED_CLEAVE_SLASH'] = Battered.BatteredCleave
 SLASH_BATTERED_CLEAVE_SLASH1 = '/BatteredCleave'
+
+SlashCmdList['BATTERED_ARMS0_SLASH'] = Battered.BatteredArms0
+SLASH_BATTERED_ARMS0_SLASH1 = '/BatteredArms0'
+
+SlashCmdList['BATTERED_ARMS2_SLASH'] = Battered.BatteredArms2
+SLASH_BATTERED_ARMS2_SLASH1 = '/BatteredArms2'
+
+SlashCmdList['BATTERED_ARMS5_SLASH'] = Battered.BatteredArms5
+SLASH_BATTERED_ARMS5_SLASH1 = '/BatteredArms5'
 
 SlashCmdList['BATTERED_ZERK0_SLASH'] = Battered.BatteredZerk0
 SLASH_BATTERED_ZERK0_SLASH1 = '/BatteredZerk0'
